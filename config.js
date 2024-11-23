ce760bc43c6099467ea29e08aa574635a2ccc2f5
Gregory Enriquez <enriquezgregory20@gmail.com>
2024-11-23 17:46:14 -0600

    Api para presupy=uestos

--- config.js
+++ config.js
@@ -1,29 +1,29 @@
-import { createPool } from "mysql2";
+import {createPool} from "mysql2";
 
-//Cargar las variables de entorno
+//Variables de entorno
 import dotenv from "dotenv";
-dotenv.config();
+ dotenv.config();
 
-//Configurar la conexion a la base de datos
-const config = createPool({
+ //configurar
+ const config = createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
     keepAliveInitialDelay: 300000,
-    enableKeepAlive: true,
-});
+    enableKeepAlive: true
 
-config.getConnection((err, connection) => {
-    if (err) { //Si hay un error
-        console.log("Error al conectar a la base de datos: ", err);
-        }
+  });
 
-        //Si no hay error
-        if(connection) connection.release();
-        console.log("Conexión exitosa a la base de datos");
-        return;
-    })
+  config.getConnection((err, connection) => {
+    if(err){
+        console.log("Error al conectar a la base de datos ", err);
+    }
+    if (connection) connection.release()
+        console.log("Conexion Exitosa")
+    return;
+    
 
-    export default config;
+ })
+ export default config;
--- index.js
+++ index.js
@@ -1,48 +1,44 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
-// Cargar las variables del entorno
-import dotenv from 'dotenv';
+//cargar las variables de entorno
+import dotenv from "dotenv";
 dotenv.config();
 
+//importar las rutas
+import gastosRouter from "./routes/gastos/gastoRoutes.js";
+import ingresosRouter from "./routes/ingresos/ingresoRoutes.js";
 
-// Importar las rutas
-import categoriasRouter from './routes/categorias/categoriasRoutes.js';
-import recetasRouter from './routes/recetas/recetasRoutes.js';
-import procedimientosRouter from './routes/procedimientos/procedimientosRoutes.js';
-import ingredientesRouter from './routes/ingredientes/ingredientesRoutes.js';
-// Crear la app de express
+//crear la app de express
 const app = express();
 
-// Habilitar la captura de datos mediante post /formularios
+//Habilitar la captura de datos mediante post / formularios
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true }));
 
-// Configurar CORS
+//configurar CORS
 const corsOptions = {
     origin: process.env.CORS_ORIGIN,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'content-Type.Authorization',
-    exposedHeaders: 'content-length,X-Knowlwdge',
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: "Content-type,Authorization",
+    exposedHeaders: "Content-Length,X-Knowledge",
     credentials: true,
     preflightContinue: false,
     optionsSuccessStatus: 204
-
 };
+
 //Apply CORS middleware globally
 app.use(cors(corsOptions));
 
-// configuracion de puerto
-const port= 3000;
+//configurar el puerto
+const port = 3000;
 
-//uso de rutas
-app.use('/categorias', categoriasRouter);
-app.use('/recetas', recetasRouter);
-app.use('/procedimientos', procedimientosRouter);
-app.use('/ingredientes', ingredientesRouter);
+//Usar las rutas
+app.use("/gastos", gastosRouter); //NOTAS
+app.use("/ingresos", ingresosRouter); //NOTAS
 
-
-//levantar el servidor en el puerto 3000
-app.listen(port,()=> {
-    console.log(`servidor corriendo en el puerto ${port}`);
+//Levantar el servidor en el puerto 3000
+app.listen(port, () => {
+    console.log(`Servidor corriendo en el puerto ${port}`);
 });
+
